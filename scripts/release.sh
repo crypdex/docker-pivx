5a96dd2b5a662d94f6ec009fd6e3c54f4469e4de
David Michael <david.michael@gmail.com>
2019-04-09 21:04:17 -0400

    consistent naming conventions

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 
-service="pivx"
+##########################################
+# Run this script with Makefile from root
+# VERSION=3.2 make release
+##########################################
+
+#VERSION=
+SERVICE="pivx"
+ARCH="arm64v8 x86_64"
+ORG=crypdex
 
 # Build and push builds for these architectures
-for arch in x86_64 arm64v8; do
-    docker build -f ${VERSION}/Dockerfile.${arch} -t crypdex/${service}:${arch}-${VERSION} -t crypdex/${service}:${arch} ${VERSION}/.
-    # Push both images, version and "latest"
-    docker push crypdex/${service}:${arch}-${VERSION}
-    docker push crypdex/${service}:${arch}
+for arch in ${ARCH}; do
+    docker build -f ${VERSION}/Dockerfile.${arch} -t ${ORG}/${SERVICE}:${VERSION}-${arch} ${VERSION}/.
+    docker push ${ORG}/${SERVICE}:${VERSION}-${arch}
 done
 
 # Now create a manifest that points from latest to the specific architecture
 rm -rf ~/.docker/manifests/*
 
 # version
-docker manifest create crypdex/${service}:${VERSION} crypdex/${service}:x86_64-${VERSION} crypdex/${service}:arm64v8-${VERSION}
-docker manifest push crypdex/${service}:${VERSION}
-
-# latest
-docker manifest create crypdex/${service}:latest crypdex/${service}:x86_64-latest crypdex/${service}:arm64v8-latest
-docker manifest push crypdex/${service}:latest
+docker manifest create ${ORG}/${SERVICE}:${VERSION} ${ORG}/${SERVICE}:${VERSION}-x86_64 ${ORG}/${SERVICE}:${VERSION}-arm64v8
+docker manifest push ${ORG}/${SERVICE}:${VERSION}
 
